6306dc88bcd72a5e0537194eed080461c581d116
mbasart <43338952+mbasart@users.noreply.github.com>
2018-09-21 10:08:39 +0200

    Add files via upload

---
+++ week 1/out/production/Figures/Figura.java
@@ -0,0 +1,13 @@
+public class Figura {
+    public double area;
+
+    public Figura(double area){
+        this.area = area;
+    }
+
+    public Figura(){
+
+        //hola holaasdkf kjaflsdk
+
+    }
+}
